85fdd5041ae9ff51f3b4d2c807723350afe08fea
RustHYL <95454209+RustHYL@users.noreply.github.com>
2023-12-16 02:58:34 +0800

    sandbox

---
+++ zhanmaoj-code-sandbox/src/test/java/com/hyl/zhanmaojcodesandbox/ZhanmaojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyl.zhanmaojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhanmaojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
